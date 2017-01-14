b3ac62fda1dd624bec41d370af0fc98b7655d2a3
César Cordero <cesar.cordero.r@gmail.com>
2017-01-14 14:40:09 -0400

    Actualización sale

--- modifications_odoo_10/models/sale_order_line_h.py
+++ modifications_odoo_10/models/sale_order_line_h.py
@@ -17,15 +17,29 @@ class SaleOrderLine(models.Model):
     
     journey = fields.Char('Journey', help='Journey')
     price_unit = fields.Float('Unit Price', required=True, digits=dp.get_precision('Product Price'), default=0.0)
-    price_unit_to_show = fields.Float('Unit Price', required=True, digits=dp.get_precision('Product Price'), default=0.0, compute='_get_price_unit_alt')
-    
-    def _get_price_unit_alt(self):
-		for record in self:
-			if record.product_id:
-				record.price_unit_to_show = record.product_id.list_price
     
     @api.onchange('product_id') # if these fields are changed, call method
     def check_change_price_unit_alt(self):
 		if self.product_id:
 			self.price_unit = self.product_id.list_price
-			self.price_unit_to_show = self.product_id.list_price
+			#self.price_unit_to_show = self.product_id.list_price
+
+    '''
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        """
+        #print "HELLO"
+        print "HELLO1 " + str(self)
+        for line in self:
+            price = line.price_unit_to_show * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_id)
+            line.update({
+                'price_tax': taxes['total_included'] - taxes['total_excluded'],
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
+            
+            print "HELLO2 " + str(line.price_unit_to_show)
+	'''
