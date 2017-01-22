8b49d3811836605513c884f7ae7a82e8ea92543c
César Cordero <cesar.cordero.r@gmail.com>
2017-01-22 18:36:47 -0400

    Actualización mail_mail

--- modifications_odoo_10/models/mail_mail_h.py
+++ modifications_odoo_10/models/mail_mail_h.py
@@ -21,26 +21,75 @@ class MailMail(models.Model):
         facilities to queue and send new email messages.  """
     _inherit = 'mail.mail'
     
-    is_user_uid = fields.Boolean('Is user UID', compute='_get_user_uid', search="_search_user_uid")
+    is_user_my_emails_uid = fields.Boolean('My emails', compute='_get_user_received_uid', search="_search_user_received_uid")
     
-    def _get_user_uid(self):
+    is_other_user_emails_uid = fields.Boolean('Other Emails', compute='_get_user_sent_uid', search="_search_user_sent_uid")
+    
+    def _get_user_received_uid(self):
         for record in self:
 			res_user_ids = self.env['res.users'].search(
 				[('id', '=', self.env.uid)]
 			)
-			if self.author_id == res_user_ids.partner_id.id:
+			emails_to = str(self.email_to).split()
+			emails_cc = str(self.email_cc).split()
+			
+			if (res_user_ids.partner_id.email in emails_to or \
+				res_user_ids.partner_id.email in emails_cc or \
+				res_user_ids.partner_id.id in self.recipient_ids) and \
+				self.state == 'received':
 				self.is_user_uid = True
 			else:
 				self.is_user_uid = False
 
-    def _search_user_uid(self, operator, value):
+    def _search_user_received_uid(self, operator, value):
+		ids=[]
+		
 		res_user_ids = self.env['res.users'].search(
 			[('id', '=', self.env.uid)]
 		)
+		
 		mail_mail_ids = self.env['mail.mail'].search(
-			[('author_id', '=', res_user_ids.partner_id.id)]
+			['|','|',('email_to', 'ilike', res_user_ids.partner_id.email),
+			('email_cc', 'ilike', res_user_ids.partner_id.email),
+			('recipient_ids', '=', res_user_ids.partner_id.id),
+			('state','=','received')]
 		)
-		return [('id', 'in', mail_mail_ids.ids)]
+		
+		ids = ids + mail_mail_ids.ids
+		
+		return [('id', 'in', ids)]
+		
+    def _get_user_sent_uid(self):
+        for record in self:
+			res_user_ids = self.env['res.users'].search(
+				[('id', '=', self.env.uid)]
+			)
+			emails_to = str(self.email_to).split()
+			emails_cc = str(self.email_cc).split()
+			
+			if (self.author_id.id == res_user_ids.partner_id.id or \
+				self.email_from == res_user_ids.partner_id.email) and \
+				self.state in ['outgoing','sent','exception']:
+				self.is_user_uid = True
+			else:
+				self.is_user_uid = False
+
+    def _search_user_sent_uid(self, operator, value):
+		ids=[]
+		
+		res_user_ids = self.env['res.users'].search(
+			[('id', '=', self.env.uid)]
+		)
+		
+		mail_mail_ids = self.env['mail.mail'].search(
+			['|',('author_id', '=', res_user_ids.partner_id.id),
+			('email_from', '=', res_user_ids.partner_id.email),
+			('state','in',['outgoing','sent','exception'])]
+		)
+		
+		ids = ids + mail_mail_ids.ids
+		
+		return [('id', 'in', ids)]
 		
     @api.onchange('author_id') # if these fields are changed, call method
     def check_change_employee(self):
